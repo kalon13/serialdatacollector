e3a0f91cb27d409cd578ffbb2df4f00f7ae186bd
d.marzioni <d.marzioni@eb2d31c8-2cfd-11df-82ed-5f3c927ac887>
2010-03-11 11:29:35 +0000

    prima

---
+++ trunk/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: studenti
+ *
+ * Created on 11 marzo 2010, 12.29
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
