dc88978830308ea14faf2e9ffa43274f1296f143
d.acca87 <d.acca87@eb2d31c8-2cfd-11df-82ed-5f3c927ac887>
2010-03-23 16:28:07 +0000

    Aggiunto l'oggetto che gestisce la camera

---
+++ serialdatacollector/src/Camera.h
@@ -0,0 +1,44 @@
+/*
+ * Camera.h
+ *
+ *  Created on: 23/mar/2010
+ *      Author: kain
+ */
+
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+#include <time.h>
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <string.h>
+#include "cv.h"
+#include "highgui.h"
+
+using namespace cv;
+
+class Camera {
+private:
+	VideoCapture *cap;
+	int wait_time;
+	time_t rawtime;
+	struct tm* leggibile;
+	struct timespec nano;
+	bool camera_flag;
+public:
+	/*
+	static VideoCapture *cap;
+	static int wait_time;
+	static time_t rawtime;
+	static struct tm* leggibile;
+	static struct timespec nano;
+	static bool camera_flag;*/
+	Camera();
+	virtual ~Camera();
+	bool open_camera(int cam, int wait);
+	void* get_photo(void* path);
+	void stop_camera();
+};
+
+#endif /* CAMERA_H_ */
